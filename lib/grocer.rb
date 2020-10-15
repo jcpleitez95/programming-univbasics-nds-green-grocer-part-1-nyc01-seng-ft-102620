d880777a3392d782b88d5955d96c62d426953ad4
Juan Carlos Pleitez <jcpleitez@outlook.com>
2020-10-15 21:11:06 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -13,7 +13,8 @@ def consolidate_cart(cart)
   counter = 0 
   while counter < cart.length do
     new_cart_item = find_item_by_name_in_collection(cart[counter][:item], new_cart)
-    if 
+    if new_cart_item != nil 
+      
 end
 
 
